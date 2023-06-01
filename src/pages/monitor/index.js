2deca72fdb0a954fd8fc3debe76c99472a214bd6
Rafi asu <eeraffi@gmail.com>
2023-06-01 19:13:41 +0700

    first commit

---
+++ src/pages/monitor/index.js
@@ -0,0 +1,23 @@
+import { Box, Flex, Image, SimpleGrid } from "@chakra-ui/react";
+import UserLayout from "../../components/layout/UserLayout";
+import shipImg from '../../components/images/dharma-ferry-8.jpg'
+import Graph, { CIIGraph } from "../../components/graph";
+
+export default function Monitor() {
+  return (
+    <UserLayout>
+      <Flex direction="column">
+        <SimpleGrid columns={2}>
+          <Box>
+            <Image src={shipImg} borderRadius="8px" w="700px" />
+          </Box>
+          <Box>
+            <CIIGraph />
+          </Box>
+        </SimpleGrid>
+
+        {/* </Flex> */}
+      </Flex>
+    </UserLayout>
+  );
+}
